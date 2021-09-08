cc62e9f112fb9670680df972a29b28ab389d01df
vilarj <lamaravillasan@outlook.com>
2021-09-07 22:38:43 -0400

    Footer with icons.

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
@@ -28,5 +31,31 @@ class Header extends React.Component {
           </ul>
         </div>
       )
+  }
+}
+
+class Body extends React.Component {
+  render () {
+    return (
+      <div className="BODY">
+        <h1></h1>
+      </div>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <div className="FOOTER">
+        <ul>
+          <a><SocialIcon url="https://twitter.com/vilarjimenez"/></a>
+          <a><SocialIcon url="https://github.com/vilarj"/></a>
+          <a><SocialIcon url="https://linkedin.com/in/vilarjimenez"/></a>
+          <a><SocialIcon url="https://www.youtube.com/channel/UCUU4YkLRdT-xnuybPSMJLpQ"/></a>
+          <a><SocialIcon url="https://discord.com/channels/@Vilarj#2029"/></a>
+        </ul>
+      </div>
+    )
   }
 }
