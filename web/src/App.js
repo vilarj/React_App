5f16f8b92ec3476683799a20a07eb1dc18d3897d
vilarj <lamaravillasan@outlook.com>
2021-09-08 22:31:15 -0400

    Footer...

--- web/src/App.js
+++ web/src/App.js
@@ -18,6 +18,15 @@ class Header extends React.Component {
   render() {
       return (
         <div className="HEADER"> 
+        </div>
+      )
+  }
+}
+
+class Body extends React.Component {
+  render () {
+    return (
+      <div className="BODY">
         <h1>About Me</h1>
         <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -32,16 +41,6 @@ class Header extends React.Component {
         <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
-        </div>
-      )
-  }
-}
-
-class Body extends React.Component {
-  render () {
-    return (
-      <div className="BODY">
-        <h1></h1>
       </div>
     )
   }
@@ -57,7 +56,7 @@ class Footer extends React.Component {
           <a><SocialIcon url="https://github.com/vilarj"/></a>
           <a><SocialIcon url="https://linkedin.com/in/vilarjimenez"/></a>
           <a><SocialIcon url="https://www.youtube.com/channel/UCUU4YkLRdT-xnuybPSMJLpQ"/></a>
-          <a><SocialIcon url="https://discord.com/channels/@Vilarj#2029"/></a>
+          <a><SocialIcon url="https://discord.com/channels/@Vilarj#2029"/></a>        
         </ul>
       </div>
     )
