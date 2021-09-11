c67c609ffe8f870c91512e2083da567a5d54c891
vilarj <lamaravillasan@outlook.com>
2021-09-10 20:18:56 -0400

    Organizing the project. Created two folders: Components and Pages to hold the appropriate data.

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css';
-import { SocialIcon } from 'react-social-icons';
+import './Header.js'
+import './Components/Body.js'
+import './Footer.js'
 
 function App() {
   return (
@@ -13,77 +15,3 @@ function App() {
 }
 
 export default App;
-
-class Header extends React.Component {
-  render() {
-      return (
-        <div className="HEADER"> 
-          <ul className="three-headers">
-            <li><a href="">About me</a></li>
-            <li><a href="">Contact Me</a></li>
-            <li><a href="">Current Project</a></li>
-            <li><a href="">Resume</a></li>
-          </ul>
-        </div>
-      )
-  }
-}
-
-class Body extends React.Component {
-  render () {
-    return (
-      <div className="BODY">
-        <div className="body-headers">
-          <div className="B1">
-            <h1>About Me</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-          </div>
-
-          <div className="B2">
-            <h1>Professional Life</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-          </div>
-          
-          <div className="B3">
-            <h1>Most Recent Project</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-          </div>
-
-        </div>
-
-        <div className="message-form">
-          <form>
-            <label>
-              Send me a message
-              <input type="text" name="name" />
-            </label>
-              <input type="submit" value="Submit" />
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
-
-class Footer extends React.Component {
-  render () {
-    return (
-      <div className="FOOTER">
-        <h3 className="social-media"> Social Media </h3>
-        <ul>
-          <a><SocialIcon url="https://twitter.com/vilarjimenez"/></a>
-          <a><SocialIcon url="https://github.com/vilarj"/></a>
-          <a><SocialIcon url="https://linkedin.com/in/vilarjimenez"/></a>
-          <a><SocialIcon url="https://www.youtube.com/channel/UCUU4YkLRdT-xnuybPSMJLpQ"/></a>
-          <a><SocialIcon url="https://discord.com/channels/@Vilarj#2029"/></a>        
-        </ul>
-      </div>
-    )
-  }
-}
---
+++ web/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+          <div className="HEADER"> 
+            <ul className="three-headers">
+              {/* <li><Link to="">About me</Link></li>
+              <li><Link to="">Contact Me</Link></li>
+              <li><Link to="">Current Project</Link></li>
+              <li><Link to="">Resume</Link></li> */}
+            </ul>
+          </div>
+        )
+    }
+}
